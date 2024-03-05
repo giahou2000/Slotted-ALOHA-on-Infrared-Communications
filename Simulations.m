163d54011bac0925f9fb52262bba6773f8ad1946
Christos Giachoudis <46033926+giahou2000@users.noreply.github.com>
2024-03-05 13:17:50 +0100

    Add files via upload

--- Simulations.m
+++ Simulations.m
@@ -10,6 +10,78 @@
 % Konstantinos Rallis,
 
 
+
+%% PSO - Particle Swarm Optimization
+% A brute force / exploration method for the optimization problem
+
+% PSO Parameters
+num_devices = 5; % Change this to the desired number of devices (K)
+num_dimensions = 3 * num_devices;
+num_particles = 30;
+max_iterations = 100;
+c1 = 2; % cognitive parameter
+c2 = 2; % social parameter
+inertia_weight = 0.7;
+
+% Initialize particles
+particles.position = rand(num_particles, num_dimensions); % Random initial positions
+particles.velocity = rand(num_particles, num_dimensions); % Random initial velocities
+particles.best_position = particles.position; % Best known positions
+% particles.best_fitness = zeros(num_particles, 1); % Best known fitness values
+
+% Evaluate fitness for each particle
+for i = 1:num_particles
+    particles.best_fitness(i) = your_objective_function(particles.best_position(i, :), num_devices);
+end
+
+% Initialize global best position and fitness
+[global_best_fitness, global_best_index] = min(particles.best_fitness);
+global_best_position = particles.best_position(global_best_index, :);
+
+% Main PSO loop
+for iteration = 1:max_iterations
+    % Update particle velocities and positions
+    for i = 1:num_particles
+        r1 = rand(1, num_dimensions);
+        r2 = rand(1, num_dimensions);
+        
+        % Update velocity
+        particles.velocity(i, :) = inertia_weight * particles.velocity(i, :) + ...
+            c1 * r1 .* (particles.best_position(i, :) - particles.position(i, :)) + ...
+            c2 * r2 .* (global_best_position - particles.position(i, :));
+        
+        % Update position
+        particles.position(i, :) = particles.position(i, :) + particles.velocity(i, :);
+        
+        % Evaluate fitness
+        current_fitness = your_objective_function(particles.position(i, :), num_devices);
+        
+        % Update personal best
+        if current_fitness < particles.best_fitness(i)
+            particles.best_fitness(i) = current_fitness;
+            particles.best_position(i, :) = particles.position(i, :);
+        end
+    end
+    
+    % Update global best
+    [min_fitness, min_index] = min(particles.best_fitness);
+    if min_fitness < global_best_fitness
+        global_best_fitness = min_fitness;
+        global_best_position = particles.best_position(min_index, :);
+    end
+    
+    % Display current best fitness value for each iteration
+    fprintf('Iteration %d: Best Fitness = %.4f\n', iteration, global_best_fitness);
+end
+
+% Display final result
+fprintf('\nFinal Result:\n');
+fprintf('Global Best Fitness = %.4f\n', global_best_fitness);
+fprintf('Global Best Position = ');
+disp(global_best_position);
+
+
+
 %% Equations
 
 % Channel Statistical Model (Gamma distribution)
@@ -18,6 +90,14 @@ x = 1:10;
 a = 13.79;
 b = 0.04;
 f_x = gampdf(x, a, b);
+
+% Your objective function (modify this for your specific problem)
+function value = your_objective_function(x, num_devices)
+    % Compute the Rk_hut and the Pk using the functions below
+    Rk_hut = 0;
+    Pk = 0;
+    value = Rk_hut/Pk;
+end
 
 % Average throughput of the network Rk_hat
 % k is the number of the kth node
